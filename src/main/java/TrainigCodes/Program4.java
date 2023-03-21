a42777ab4456b1659ba517dc3a8bcdf49ae1d782
shra-1234 <shraddhamath19@gmail.com>
2023-03-21 08:03:36 +0530

    Program to display 1 to 25 in form of matrix

---
+++ src/main/java/TrainigCodes/Program4.java
@@ -0,0 +1,15 @@
+package TrainigCodes;
+
+public class Program4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=1;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print(count+" ");
+				count++;
+			}System.out.println();
+		}
+	}
+
+}
